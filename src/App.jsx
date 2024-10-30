d4dbe75cc7ba704af4946fa3a77a3b0afe3933de
Carlie <carliemaloneco@gmail.com>
2024-10-29 21:02:48 -0600

    saving prog

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import './input.css';
 import NavBar from './Components/NavBar.jsx';
+import Fireworks from './Components/Animation/Fireworks.jsx'
+import Flower from './Components/Flower.jsx';
+import BG from './assets/background.jpg';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,9 +11,16 @@ function App() {
   return (
     <>
       <NavBar />
-      <div className='container'>
-        <p className='pt-32 flex justify-center'>test</p>
-      </div>
+      <section>
+        <Fireworks />
+        {/* <img src={BG} alt='img of downtown Denver, CO' rel='norel' className='z-0 absolute h-[95vh] overflow-hidden w-screen min-w-[1200px] object-cover'/> */}
+        <div className='container'>
+          <p className='pt-32 flex justify-center'>test</p>
+        </div>
+        <div className='py-40'>
+          {/* <Flower /> */}
+        </div>
+      </section>
     </>
   )
 }
