d6909f161dca1a746f3692c74ff3af143b26b63e
Carlie <carliemaloneco@gmail.com>
2024-10-30 12:46:02 -0600

    cleaned crafts

--- src/Crafts.jsx
+++ src/Crafts.jsx
@@ -11,11 +11,11 @@ const Classes = () => {
     return (
         <section id='#Crafts'>
             <div className='container'>
-                    <div className='flex-wrap mb-14 mx-24 relative'>
-                        <h1 className='pt-24 text-black text-[65px] flex stroke-black font-medium flex-row-reverse'>What we Offer</h1>
-                        <span className='absolute w-[5.5rem] h-[4px] my-[-15px] bg-[#949978] right-0'/>
+                    <div className='flex-wrap mb-14 mx-12 relative'>
+                        <h1 className='pt-12 text-black text-[65px] flex stroke-black font-light'>What we Offer</h1>
+                        <span className='absolute w-[5.5rem] h-[4px] my-[-15px] bg-[#f3c143]'/>
                     </div>
-                    <div className='cardContainer'>
+                    <div className='cardContainer pb-12'>
                         {classes.map((info) => (
                             <div key={info.id} className={info.id % 2 !== 0 ? 'card-deck' : 'card-deck pt-24'}>
                                 <div className='card'>
