12a573489e6dc5d71b4b2417a504fb66b8667719
Carlie <carliemaloneco@gmail.com>
2025-06-22 13:03:51 -0600

    saving prog

--- src/Info.jsx
+++ src/Info.jsx
@@ -6,13 +6,13 @@ const Info = () => {
     return (
         <section id='Info'>
             <div className='container'>
-                <div className='flex justify-center'>
+                <div className='flex justify-center pt-16'>
                     <div className='flex gap-20'>
                         <img className='img-about' src={Test} alt='test pic'/> 
                             <div className='flex-wrap'>
                                 <h4 className='text-[50px] font-thin pt-24 overflow-hidden'>Shop!</h4>
-                                <p className='text-about'>As of recent, Apple Crafts has expanded into a boutique located at 
-                                    <p className='font-bold text-center text-[30px]'>Grounds for Dismissal</p> 
+                                <p className='text-[25px] max-sm:text-center max-sm:text-[1rem] max-sm:pb-3 max-sm:px-3'>As of recent, Apple Crafts has expanded into a boutique located at 
+                                    <p className='font-bold text-center text-[30px] max-sm:pb-3 max-sm:px-3'>Grounds for Dismissal</p> 
                                 located in Aurora, CO! Feel free to stop by, enjoy some coffee and forage for a new statement piece!</p>
                                 {/* <p className='text-about'>I specialize in designing dynamic and captivating interfaces, employing clean and efficient coding practices, and harnessing the latest development tools and methodologies. Additionally, I am a collaborative team player, thriving in cross-functional environments and contributing to the development of outstanding web applications.</p> */}
                             </div>
